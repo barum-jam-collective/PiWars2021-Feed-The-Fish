18a6ce1f32456740392d2e942595b5ebdcf9da3d
JonnyAlpha <billharvs@btinternet.com>
2021-06-13 13:54:44 +0100

    Update and rename feed_fish_nerf_gpiozero.py to feed_fish_gpiozero.py

--- feed_fish_gpiozero.py
+++ feed_fish_gpiozero.py
@@ -1,20 +1,26 @@
 # Feed The Fish - Manual Control for driving and firing using DIY NERF launcher motors controlled using MOSFET and GPIOZero PWMLED function
 # Bill Harvey 16 Feb 2021
-# Last update 31 May 2021
+# Last update 13 June 2021
+
+# Current list of in use servos - identify max an min using ultra_gui.py
+# Servo 1 = fire breach mechanism PS4 Controller Crosses 'X' button
+# Servo 2 = Pan left and right - PS4 Controller Right Analogue y axis
+# Servo 3 = Tilt up and down (63 Max up, 0.44 max down) PS4 Controler Right analogue x axis
+
 
 from gpiozero import PWMLED, Servo
 from time import sleep
-from approxeng.input.selectbinder import ControllerResource # Import Approx Eng Controller libraries
+from approxeng.input.selectbinder import ControllerResource  # Import Approx Eng Controller libraries
 import ThunderBorg3 as ThunderBorg
 import UltraBorg3 as UltraBorg
-#import os
+# import os
 import sys
 
 global TB
 
 # Setup the ThunderBorg
 TB = ThunderBorg.ThunderBorg()
-#TB.i2cAddress = 0x15                 # Uncomment and change the value if you have changed the board address
+# TB.i2cAddress = 0x15                 # Uncomment and change the value if you have changed the board address
 TB.Init()
 if not TB.foundChip:
     boards = ThunderBorg.ScanForThunderBorg()
@@ -40,38 +46,40 @@ if not failsafe:
 
 TB.MotorsOff()
 TB.SetLedShowBattery(False)
-TB.SetLeds(0,0,1)
+TB.SetLeds(0, 0, 1)
 
 # Start the UltraBorg
-UB = UltraBorg.UltraBorg()      # Create a new UltraBorg object
-UB.Init()                       # Set the board up (checks the board is connected)
+UB = UltraBorg.UltraBorg()  # Create a new UltraBorg object
+UB.Init()  # Set the board up (checks the board is connected)
 
 # Set servo to centre
-UB.SetServoPosition4(-0.02) #Test Servo positioning using ultra_gui.py to obtain start position and insert here
+UB.SetServoPosition4(-0.02)  # Test Servo positioning using ultra_gui.py to obtain start position and insert here
 
 # Setup Nerf Launcher motor variable (BCM numbering system)
 global launcher_motors
 global launcher_on
 
 # Setup the launcher motor settings
-launcher = LED(17) # Assign GPIO pin 17 to control PWM for launcher motor 
-launcher.value = 0 #ensure launcher motors are off
+launcher = PWMLED(17)  # Assign GPIO pin 17 to control PWM for launcher motor
+launcher.value = 0  # ensure launcher motors are off
 
-# Setup the launcher loading mechanism setting  
-breach = Servo(25) # Assign GPIO pin 25 to control breach block servo
+# Setup the launcher loading mechanism setting
+breach = Servo(25)  # Assign GPIO pin 25 to control breach block servo
 
 # Set min and max travel for breacj block servo
-global breach_min = -0.8
-global breach_max = 0.3
+breach_min = -0.6
+breach_max = 0.3
 
 def set_speeds(power_left, power_right):
-    TB.SetMotor1(power_left/100)
-    TB.SetMotor2(power_right/100)
+    TB.SetMotor1(power_left / 100)
+    TB.SetMotor2(power_right / 100)
+
 
 def stop_motors():
     TB.MotorsOff()
 
-def mixer(yaw, throttle, max_power=100):
+
+def mixer(yaw, throttle, max_power=50): # was 100, reduced to 50 to see if it helps control
     """
     Mix a pair of joystick axes, returning a pair of wheel speeds. This is where the mapping from
     joystick positions to wheel powers is defined, so any changes to how the robot drives should
@@ -86,29 +94,54 @@ def mixer(yaw, throttle, max_power=100):
         A pair of power_left, power_right integer values to send to the motor driver
     """
 
-    left = throttle - yaw # was +
-    right = throttle + yaw # was -
+    left = throttle - yaw  # was +
+    right = throttle + yaw  # was -
     scale = float(max_power) / max(1, abs(left), abs(right))
     return int(left * scale), int(right * scale)
 
-def start_launcher_motors():
+#def aim(aim_x, aim_y):
+
+#    UB.SetServoPosition3(aim_y)
+
+def aim_left_right(aim):
+    UB.SetServoPosition2(aim)
+
+def elevation_up_down(elevation):
+    UB.SetServoPosition3(elevation)
+
+def start_launcher_motors(launcher_on):
     print("Starting launcher Motors")
-    print("Press X to fire and Cross to stop")
+    print("Press X to fire and Circle to stop")
     launcher.value = 0.33
+    return launcher_on
 
 def fire():
+    global breach_min
+    global breach_max
     # Activate loading servo
-    UB.SetServoPosition4(breach_min) #was Test Servo positioning using ultra_gui.py to obtain start position and insert here
-    sleep(0.5) # Insert loading time here
-    UB.SetServoPosition4(breach_max) #was -0.47 Test Servo positioning using ultra_gui.py to obtain loading position and insert here
+    UB.SetServoPosition4(
+        breach_min)  # was Test Servo positioning using ultra_gui.py to obtain start position and insert here
+    sleep(0.5)  # Insert loading time here
+    UB.SetServoPosition4(
+        breach_max)  # was -0.47 Test Servo positioning using ultra_gui.py to obtain loading position and insert here
     sleep(0.5)
-    UB.SetServoPosition4(breach_min) #Test Servo positioning using ultra_gui.py to obtain start position and insert here
+    UB.SetServoPosition4(
+        breach_min)  # Test Servo positioning using ultra_gui.py to obtain start position and insert here
+
 
 def stop_launcher_motors():
     print("Stopping launcher Motors")
     launcher.value = 0
 
+
 def main():
+    global breach_min
+    global breach_max
+    global launcher_on
+    global aim
+    global elevation
+    aim = 0
+    elevation = 0
     print("Drive using left hand joystick")
     print("Press square to start launcher motors")
     print("Press Circle to stop launcher motors")
@@ -128,45 +161,77 @@ def main():
 
                         # Set motor speeds
                         set_speeds(power_left, power_right)
-                        
-                        # Get a ButtonPresses object containing everything that was pressed since the last iteration of the loop
-                        joystick.check_presses()
-                        # Print any buttons that were pressed
-                        if joystick.has_presses:
-                            print(joystick.presses)
-                        
-                        #left_y = joystick["ly"]
-                        #print("Left Joy")
-                        #right_y = joystick["ry"]
-                        #print("Right Joy")
-                        #driveLeft = left_y
-                        #driveRight = right_y
 
-                        #TB.SetMotor1(driveRight)
-                        #TB.SetMotor2(driveLeft)
+                        # Get aiming left, right, up and down
+                        #aim_x, aim_y = joystick['rx', 'ry']
+
+                        # Set servo 2 and 3 positions for aiming
+                        #aim(aim_x, aim_y)
+
+
+                        # Get a ButtonPresses object containing everything that was pressed since the last iteration of the loop
+                        #joystick.check_presses()
+                        # Print any buttons that were pressed
+                        #if joystick.has_presses:
+                        #    print(joystick.presses)
+
+                        # left_y = joystick["ly"]
+                        # print("Left Joy")
+                        # right_y = joystick["ry"]
+                        # print("Right Joy")
+                        # driveLeft = left_y
+                        # driveRight = right_y
+
+                        # TB.SetMotor1(driveRight)
+                        # TB.SetMotor2(driveLeft)
 
                         # Read the buttons to determine Nerf launcher controls
                         presses = joystick.check_presses()
+                        launcher_on = ""
                         if presses.square:
                             print("Square pressed")
                             # Start launcher motors
                             launcher_on = "yes"
-                            start_launcher_motors()
+                            start_launcher_motors(launcher_on)
                             # Fire NERF
                             # Need to add some error checking here to prevent firing if motors are nut turning?
 
-                        elif presses.cross
-                            print("Circle Pressed")
-                            if launcher_on == "yes":
-                                fire()
-                            else:
-                                print("Motors aren't running, press 'Square' to start")
+                        if presses.cross:
+                            print("Firing")
+                            print(launcher_on)
+                            fire()
+                            #if launcher_on == "yes":
+                            #    fire()
+                            #else:
+                            #    print("Motors aren't running") #, press 'Square' to start")
 
-                        elif presses.circle:
+                        if presses.circle:
                             # Stop launcher motors
                             print("Circle pressed")
                             launcher = "No"
                             stop_launcher_motors()
+
+                        # aiming
+
+                        if presses.l1:
+                            # Pan cannon left
+                            aim +=0.1
+                            aim_left_right(aim)
+
+                        if presses.r1:
+                            # Pan cannon right
+                            aim -=0.1
+                            aim_left_right(aim)
+
+                        if presses.l2:
+                            # Raise cannon
+                            elevation +=0.05
+                            elevation_up_down(elevation)
+
+                        if presses.r2:
+                            # Lower cannon
+                            elevation -=0.05
+                            elevation_up_down(elevation)
                 # Joystick disconnected.....
                 print("Connection to joystick lost")
                 stop_launcher_motors()
@@ -176,17 +241,18 @@ def main():
                 # No joystick found, wait for a bit and try again
                 print("No joysticks found")
                 # Set LEDs blue
-                TB.SetLeds(0,0,1)
+                TB.SetLeds(0, 0, 1)
                 sleep(1.0)
         except KeyboardInterrupt:
             # CTRL+C exit, give up
             print("\nUser aborted")
             TB.MotorsOff()
             TB.SetCommsFailsafe(False)
-            TB.SetLeds(0,0,0)
+            TB.SetLeds(0, 0, 0)
             motors.off()
-            #motor1.stop()
-            #motor2.stop()
+            # motor1.stop()
+            # motor2.stop()
             sys.exit()
+
 
 main()
